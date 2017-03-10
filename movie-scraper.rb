fc8430e37d6ac5b2cf2a1f65d51f2369a487188f
DanL12186 <DanL12186@users.noreply.github.com>
2017-03-10 16:16:17 -0500

    Fixed(?) scraper...
    Seems to scrape and properly create objects with properties (name/genre/stars/summary).

--- movie-scraper.rb
+++ movie-scraper.rb
@@ -3,24 +3,27 @@ NewMovies::Movie_Scraper
 class Movie_Scraper
   @@all = []
 
-  attr_reader :name, :starring, :summary
+  attr_accessor :starring, :summary, :name, :genre
 
   def initialize(name="")#, starring=[], summary="")
-    @name = self.namescrape[movie_names]
+    @name = name
     # @starring = starring
-    # @summary = self.namescrape[summaries]
+    # @summary = summary
     @@all << self
   end
 
-
-  def self.namescrape
+  def self.namescrape #div.panel.each do
     page = Nokogiri::HTML(open('http://www.movieinsider.com/movies/coming-soon')) #movieinsider/new-movies main
-    movie_names = page.css('h4.media-heading a').text.strip.split("  ") #grabs names.to_a ["Harry Potter", "The Bourne Identity", "Serenity", "The Matrix,"] etc.
-    movie_names.collect {|name| NewMovies::Movie_Scraper.new(name)} #should create a new Movie_Scraper object w/name for each on list
-    # starring = page.css('p.credits').text.strip.split("   ") #isolates actors by groups (actors per movie)
-    #summaries = page.css('div.show-more-hidden p.media-content').text.strip.split("  ") #grabs movie summaries
-    # #ratings =       #s.scan(/\d+/).first (mpaa rating)
-    # genres = page.css('p span.label').text.strip
+    page.css('div.panel').collect do |mov|
+      movie = Movie_Scraper.new
+      movie.name= mov.css('h4.media-heading a').text.strip.split("  ")#, page.css('p span.label').text.strip  #grabs names.to_a ["Harry Potter", "The Bourne]
+      movie.starring= mov.css('p.credits').text.strip.split("   ") #isolates actors by groups (actors per movie)
+      movie.summary = mov.css('div.show-more-hidden p.media-content').text.strip.split("  ") #grabs movie summaries
+      movie.genre = mov.css('p span.label').text.strip
+      #ratings =       #s.scan(/\d+/).first (mpaa rating)
+    #self.all
+    movie
+    end
   end
 
   # def name=(name)
@@ -31,7 +34,6 @@ class Movie_Scraper
   def self.all
     @@all
   end
-
 
 
 end
