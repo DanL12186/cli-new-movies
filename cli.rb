528b2f4c9aff4853dc9f4d61f190585127043f2b
DanL12186 <DanL12186@users.noreply.github.com>
2017-03-11 17:19:29 -0500

    added "call" and a "display" outlines to CLI
    Non-functional at this point.

--- cli.rb
+++ cli.rb
@@ -1,6 +1,14 @@
 class NewMovies::CLI
 
-  puts
-  puts "#{movie.name} (#{movie.genre}) \nStarring: #{movie.starring} \n\nSummary: #{movie.summary}."
+  def call
+    puts "Movie List:"
+    puts
+    movie_list  #wiil call a list of all movies in a particular visual format
+    display_movie_details #calls below method in some future form
+  end
+
+  def display_movie_details
+    puts "#{mov.name} (#{mov.genre}) \nStarring: #{mov.starring} \n\nSummary: #{mov.summary}."
+  end
 
 end
