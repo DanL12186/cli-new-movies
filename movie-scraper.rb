17b63c04e3e38a731c01209983f854ee979bb3e4
DanL12186 <DanL12186@users.noreply.github.com>
2017-03-11 11:27:53 -0500

    forgot to save file before uploading

--- movie-scraper.rb
+++ movie-scraper.rb
@@ -16,15 +16,15 @@ class Movie_Scraper
     page = Nokogiri::HTML(open('http://www.movieinsider.com/movies/coming-soon')) #movieinsider/new-movies main
 
     page.css('div.panel').collect do |mov| #moov
-      movie = Movie_Scraper.new()
+      movie = Movie_Scraper.new() #will of course have to change this to NewMovies::Movie_Scraper later
 
-      movie.name= mov.css('h4.media-heading a').text.strip.split("  ")#, page.css('p span.label').text.strip  #grabs names.to_a ["Harry Potter", "Serenity"]
+      movie.name= mov.css('h4.media-heading a').text.strip.split("  ").join#, page.css('p span.label').text.strip  #grabs names.to_a ["Harry Potter", "Serenity"]
       movie.starring= mov.css('p.credits').text.strip.split("   ") #isolates actors by groups (actors per movie)
-      movie.summary = mov.css('div.show-more-hidden p.media-content').text.strip.split("  ") #grabs movie summaries
+      movie.summary = mov.css('div.show-more-hidden p.media-content').text.strip.split("  ").join #grabs movie summaries
       movie.genre = mov.css('p span.label').text.strip
-      movie.rating = mov.css('ins.mpaa').to_s.scan(/\d+/) #(mpaa rating)
-    #self.all
-    movie
+      movie.rating = mov.css('ins.mpaa').to_s.scan(/\d+/).join.to_i #(mpaa rating)
+      #self.all
+      movie
     end
   end
 
