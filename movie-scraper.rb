9bb71c48c22a1fd94153142cbcaa816fa7302899
DanL12186 <DanL12186@users.noreply.github.com>
2017-03-11 10:54:31 -0500

    Added MPAA scrape
    Seems to be working properly. Will just have to convert numbers to ratings; 1==> G,  2==> PG,  3==> PG-13,  4==>R.

--- movie-scraper.rb
+++ movie-scraper.rb
@@ -3,10 +3,10 @@ NewMovies::Movie_Scraper
 class Movie_Scraper
   @@all = []
 
-  attr_accessor :starring, :summary, :name, :genre
+  attr_accessor :starring, :summary, :name, :genre, :rating
 
-  def initialize(name="")#, starring=[], summary="")
-    @name = name
+  def initialize#(name="")#, starring=[], summary="")
+    #@name = name
     # @starring = starring
     # @summary = summary
     @@all << self
@@ -14,22 +14,19 @@ class Movie_Scraper
 
   def self.namescrape #div.panel.each do
     page = Nokogiri::HTML(open('http://www.movieinsider.com/movies/coming-soon')) #movieinsider/new-movies main
-    page.css('div.panel').collect do |mov|
-      movie = Movie_Scraper.new
-      movie.name= mov.css('h4.media-heading a').text.strip.split("  ")#, page.css('p span.label').text.strip  #grabs names.to_a ["Harry Potter", "The Bourne]
+
+    page.css('div.panel').collect do |mov| #moov
+      movie = Movie_Scraper.new()
+
+      movie.name= mov.css('h4.media-heading a').text.strip.split("  ")#, page.css('p span.label').text.strip  #grabs names.to_a ["Harry Potter", "Serenity"]
       movie.starring= mov.css('p.credits').text.strip.split("   ") #isolates actors by groups (actors per movie)
       movie.summary = mov.css('div.show-more-hidden p.media-content').text.strip.split("  ") #grabs movie summaries
       movie.genre = mov.css('p span.label').text.strip
-      #ratings =       #s.scan(/\d+/).first (mpaa rating)
+      movie.rating = mov.css('ins.mpaa').to_s.scan(/\d+/) #(mpaa rating)
     #self.all
     movie
     end
   end
-
-  # def name=(name)
-  #   @name= self.namescrape
-  # end
-
 
   def self.all
     @@all
