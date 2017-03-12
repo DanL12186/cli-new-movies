9ded15bc52e6071c530197b1cebf601beca67db5
DanL12186 <DanL12186@users.noreply.github.com>
2017-03-12 13:29:51 -0400

    began building display_movie_details method

--- cli.rb
+++ cli.rb
@@ -1,13 +1,21 @@
 class NewMovies::CLI
 
   def call
+    puts
     puts "Movie List:"
     puts
     movie_list  #wiil call a list of all movies in a particular visual format
-    display_movie_details #calls below method in some future form
+    display_movie_details #calls display_movie_details in some future form
   end
 
   def display_movie_details
+    input = nil
+    puts "Enter the number of the movie you want more info on, or type 'exit' to quit."
+
+    until input == "exit" || "Exit"
+    input = gets.strip
+
+    puts
     puts "#{mov.name} (#{mov.genre}) \nStarring: #{mov.starring} \n\nSummary: #{mov.summary}."
   end
 
