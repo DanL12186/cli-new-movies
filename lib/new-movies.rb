94033f1ec35fe12faf9017f930f6f933e4f15aec
DanL12186 <DanL12186@users.noreply.github.com>
2017-03-15 13:57:39 -0400

    starting over using bundler...

---
+++ lib/new-movies.rb
@@ -0,0 +1,11 @@
+require "new_movies/version"
+require_relative "./new_movies/movie-scraper"
+require_relative "./new_movies/cli"
+
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+module NewMovies
+  # Your code goes here...
+end
