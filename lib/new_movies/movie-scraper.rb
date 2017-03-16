954b49def463894a4c1f13ed900aede1162aa296
DanL12186 <DanL12186@users.noreply.github.com>
2017-03-16 12:09:26 -0400

    *Really* fixed short-summary issue and...
    Turned mpaa if/elsif nest into a case statement which reads slightly better.  Used a pretty ugly lambda to fix the issue with short summaries not being shown. Tried single-line if/else logic that didn't work. Could certainly do multiline if/else, but in the interest of space, I think not for now. Could be implemented in the CLI instead (as could mpaa).

--- lib/new_movies/movie-scraper.rb
+++ lib/new_movies/movie-scraper.rb
@@ -15,31 +15,31 @@ class NewMovies::Movie_Scraper
 
       movie.name     =  mov.css('h4.media-heading a').text.strip
       movie.starring =  mov.css('p.credits').text.strip
-      movie.summary  =  mov.css('div.show-more-hidden p.media-content').text.strip #issue: short summaries not listed due to using diff div.
+      movie.summary  =  mov.css('div.show-more-hidden p.media-content').text.strip
       movie.genre    =  mov.css('p span.label').text.strip
       movie.rating   =  mov.css('ins.mpaa').to_s.scan(/\d/).join #mpaa rating as "int"
 
       movie.mpaa #calls mpaa method and converts all ratings from string-nums==> ratings
+      movie.summary == "" ? movie.summary = mov.css('p.media-content').text.strip : movie.summary = movie.summary #ugly :/
     end
    self.all
   end
 
   def mpaa #would prefer to map to a hash if I knew how.
     @@all.each do |mov|
-      if mov.rating.empty? #class-specific. blank didn't work. works for empty strings.
+      case mov.rating
+      when ''
         mov.rating = "No rating available"
-      elsif mov.rating == '1'
+      when '1'
         mov.rating = "G"
-      elsif mov.rating == '2'
+      when '2'
         mov.rating = "PG"
-      elsif mov.rating == '3'
+      when '3'
         mov.rating = "PG-13"
-      elsif mov.rating == '4'
+      when '4'
         mov.rating = "R"
-      elsif mov.rating == '6'
+      when '6'
         mov.rating = "Unrated"
-      else
-        mov.rating = mov.rating
       end
     end
   end
