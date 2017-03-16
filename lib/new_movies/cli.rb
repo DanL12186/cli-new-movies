b51f61eddeb246bb9ec3593f40905d10c4b6db23
DanL12186 <DanL12186@users.noreply.github.com>
2017-03-16 13:02:38 -0400

    added "list" and error-catching functionality
    (CLI no longer crashes due to an unrecognized command or a number outside the range of the movie list). CLI still #puts the movie details of the last movie on the list on exit, though.

--- lib/new_movies/cli.rb
+++ lib/new_movies/cli.rb
@@ -2,8 +2,8 @@ class NewMovies::CLI
 
   def call
     NewMovies::Movie_Scraper.namescrape #was handled by the @@all ||= scrape in the example. Probably shouldn't *really* have this here, but...
-    movie_list  #calls a list of all movies; Movie (Genre)
-    display_movie_details #calls display_movie_details in some future form
+    movie_list  #x. Movie (Genre)
+    display_movie_details
   end
 
   def movie_list
@@ -15,19 +15,23 @@ class NewMovies::CLI
     end
   end
 
-  def display_movie_details
+  def display_movie_details #this badly needs to be cleaned up and and abstracted. Too much unrelated funcationality going on in one method.
     input = nil
     puts
     puts "Enter the number of the movie you want more info on, or type 'exit' to quit."
     puts
+
     until input == "exit"
     input = gets.strip.downcase
 
     mov = NewMovies::Movie_Scraper.all[input.to_i - 1] #rn input is, e.g., 2 ("3" => 2)
 
-    puts
-    puts "#{mov.name} (#{mov.genre}) - Starring: #{mov.starring} \n\nSummary: #{mov.summary}."
-    puts
+    if input != 'exit' && input != "list" && !input.to_i.between?(1,(NewMovies::Movie_Scraper.all.length))
+      puts "\nUnrecognized command. Enter the number of the movie you wish to learn more about, type 'list' to view list, or 'exit' to quit.\n"
+    else
+      puts
+      puts "#{mov.name} (#{mov.genre}) - Starring: #{mov.starring} \n\nSummary: #{mov.summary}"
+      puts
 
     if mov.rating == "No rating available"
       puts mov.rating
@@ -35,9 +39,16 @@ class NewMovies::CLI
       puts "Rated #{mov.rating}"
     end
 
-    puts
-    puts "Would you like to see another movie? Enter its number here:"
-    puts
+    if input == 'list'
+      movie_list
+      puts
+      puts "Enter the number of the movie you want more info on, or type 'exit' to quit."
+      puts
+    else
+      puts
+      print "Would you like to see another movie? Enter its number here: "
+      end
     end
   end
+end
 end
